21f95bb3f7d13b8410cc7815b40dbd673e013909
Pandra14 <130453008+Pandra14@users.noreply.github.com>
2023-04-17 09:51:57 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -26,6 +26,42 @@ input.onButtonPressed(Button.A, function () {
         basic.pause(100)
     }
 })
+input.onButtonPressed(Button.B, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showLeds(`
+            . . . . .
+            . . # . .
+            . # # # .
+            . . # . .
+            . . . . .
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # # # # #
+            . . # . .
+            . . # . .
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            . . # . .
+            . . . . .
+            # . . . #
+            . . . . .
+            . . # . .
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(100)
+    }
+})
 basic.forever(function () {
 	
 })
